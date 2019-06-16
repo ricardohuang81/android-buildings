1f543ffcbbc0317ed0e9a41796a9103ba88cdaa2
Ricky Huang <huangricky@gmail.com>
2019-06-16 17:37:36 -0400

    first commit for android assignment

---
+++ app/src/main/java/qcc/cuny/ch5_huang/taipei101_activity.java
@@ -0,0 +1,13 @@
+package qcc.cuny.ch5_huang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class taipei101_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taipei101_activity);
+    }
+}
